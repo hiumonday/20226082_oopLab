ed37e2c1fc6cf1150fb2eb86a7dd5f00eae7e8c6
hiumonday <134221779+hiumonday@users.noreply.github.com>
2024-11-26 04:28:23 +0000

    class

--- Lab03/AimsProject/src/Aims.java
+++ Lab03/AimsProject/src/Aims.java
@@ -1,22 +1,44 @@
 public class Aims {
     public static void main(String[] args) {
-        Customer customer = new Customer("Hieu", "hieunt@gmail.com", "Hadong", "Email");
-
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion", "Animation", "Roger Allers", 87, 19.95f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
-
         Cart cart = new Cart();
-        cart.addDigitalVideoDisc(dvd1);
-        cart.addDigitalVideoDisc(dvd2);
-        cart.addDigitalVideoDisc(dvd3);
-        cart.removeDigitalVideoDisc(dvd2);
 
-        //Add to custommer cart
-        customer.addToOrderHistory(cart);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Animation", "Frozen", 19.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("James Cameron", "Action", "Avatar", 24.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Sci-Fi", "Star Wars", 20.00f);
 
-        //Display customer cartt
-        customer.displayCustomerInfo();
-        System.out.println("Total cost: " + cart.totalCost());
+        // Display information
+        System.out.println("DVD 1: " + dvd1.getTitle() + ", ID: " + dvd1.getId());
+        System.out.println("DVD 2: " + dvd2.getTitle() + ", ID: " + dvd2.getId());  
+        System.out.println("DVD 3: " + dvd3.getTitle() + ", ID: " + dvd3.getId());
+        System.out.println("DVD 4: " + dvd4.getTitle() + ", ID: " + dvd4.getId());
+        // Display total number of DVDs created
+        System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+
+
+//        // Test adding a single DVD
+//        cart.addDigitalVideoDisc(dvd1);
+//
+//        // Test adding two DVDs
+//        cart.addDigitalVideoDisc(dvd2, dvd3);
+//
+//        // Test adding an array of DVDs
+//        hust.soict.dsai.aims.disc.DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3, dvd4};
+//        cart.addDigitalVideoDisc(dvdList);
+//
+//        // Print total cost to verify
+//        System.out.println("Total cost is: " + cart.totalCost());
+//
+//        // Print all DVDs in the cart
+//        cart.printCart();
+
+
+//        cart.addDigitalVideoDisc(dvd1);
+//        cart.addDigitalVideoDisc(dvd2);
+//        cart.addDigitalVideoDisc(dvd3);
+//
+//        cart.removeDigitalVideoDisc(dvd2);
+//
+//        System.out.println("Total cost is: " + cart.totalCost());
     }
 }
